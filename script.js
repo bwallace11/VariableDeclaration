070ca58eaf198785f89dc287f77e93e774c9f5c9
Brittany <bwallace11@ewu.edu>
2026-01-22 18:27:55 +0000

    done

---
+++ script.js
@@ -0,0 +1,6 @@
+// declare a variable named `name`
+// assign value of `Eastern Washington University`
+let name = "Eastern Washington University";
+// 🛑 DO NOT EDIT below this line
+const h1 = document.querySelector("h1");
+h1.textContent = name;
